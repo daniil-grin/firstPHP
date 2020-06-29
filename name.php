bdb310693bd10c072dad53a321029032c399eec2
Danny <dania2010-01@mail.ru>
2020-06-29 10:19:42 +0500

    Passing variables

---
+++ name.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $name = $_GET['name'];
+  echo 'Добро пожаловать на наш веб-сайт, '.$name.'!';
+  ?>
+</body>
+</html>
