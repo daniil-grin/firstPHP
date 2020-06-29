deca11f8e470c8e8b9e5769266b76778fe245a7f
Danny <dania2010-01@mail.ru>
2020-06-29 10:10:14 +0500

    Today's date

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Сегодняшняя дата</title>
+</head>
+<body>
+	<p>Сегодняшняя дата: 
+		<?php
+		echo date('l, F jS Y.');
+		?>
+  </p>
+</body>
+</html>
