4212ba7ba0827c31f0ffc8dbdc4c891772d4c177
Danny <dania2010-01@mail.ru>
2020-06-29 10:44:43 +0500

    Passing variables v2

--- name.php
+++ name.php
@@ -7,7 +7,7 @@
 <body>
   <?php
   $name = $_GET['name'];
-  echo 'Добро пожаловать на наш веб-сайт, '.$name.'!';
+  echo 'Добро пожаловать на наш веб-сайт, '.htmlspecialchars($name,ENT_QUOTES, 'UTF-8').'!';
   ?>
 </body>
 </html>
