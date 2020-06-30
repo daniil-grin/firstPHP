e05547b04deb57a71964f30457973922d8e84b62
Danny <dania2010-01@mail.ru>
2020-06-30 11:11:01 +0500

    Deleting Information

---
+++ deletejoke/form.html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Добавление шутки</title>
+  <style type="text/css">
+  textarea{
+    display: block;
+    width: 100%;
+  }
+  </style>
+</head>
+<body>
+  <form action="?" method="post">
+    <div>
+      <label for="joketext">Введите сюда свою шутку:</label>
+      <textarea id="joketext" name="joketext" rows="3" cols="40">
+      </textarea>
+    </div>
+    <div><input type="submit" value="Добавление"></div>
+  </form>
+</body>
+</html>
